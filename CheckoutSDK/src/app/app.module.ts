cdef73b5da0c37e4d6d948b47c433427f0599a4e
Gunnar Sireus <gu0628174@cidronpay.com>
2018-11-08 13:29:06 +0100

    Routes working

--- CheckoutSDK/src/app/app.module.ts
+++ CheckoutSDK/src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule } from '@angular/common/http';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InlineComponent } from './inline/inline.component';
+import { RedirectComponent } from './redirect/redirect.component';
+import { ModalComponent } from './modal/modal.component';
+
+
+const routes : Routes = [
+  {path: 'redirect', component:RedirectComponent},
+  {path: 'modal', component:ModalComponent},
+  {path: 'inline', component:InlineComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InlineComponent,
+    RedirectComponent,
+    ModalComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    RouterModule.forRoot( 
+      routes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
