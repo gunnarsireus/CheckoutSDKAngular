ef58ba0650ed88f3f4319324bc5755a66c7e7f40
Gunnar Sireus <gu0628174@cidronpay.com>
2018-11-09 08:21:54 +0100

    Inline and Modal working

--- CheckoutSDK/src/app/inline/inline.component.ts
+++ CheckoutSDK/src/app/inline/inline.component.ts
@@ -1,15 +1,93 @@
 import { Component, OnInit } from '@angular/core';
+import { InlineCheckout, Event} from "@bambora/checkout-sdk-web";
 
 @Component({
   selector: 'app-inline',
-  templateUrl: './inline.component.html',
+  template: `<button (click)="buttonClicked()">Betala inline</button>`,
   styleUrls: ['./inline.component.css']
 })
 export class InlineComponent implements OnInit {
+  parentUrl:string;
+  constructor(){
+      this.parentUrl='empty url'
+   }
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit(){
   }
 
+  ngOnDestroy(){
+    this.clearScreen();
+  }
+
+  clearScreen(){
+    var iframes = document.getElementsByTagName('iframe');
+    for (var i = 0; i < iframes.length; i++) {
+            iframes[i]!.parentNode!.removeChild(iframes[i]);
+     }
+  }
+
+
+  buttonClicked() {
+    let accessToken = "M09TiRC9emeAdBYAdbhV";
+    let merchantNumber = "T064742601";
+    let secretToken = "yfxJkjkGexrWbmYBrb8hVrsAVWjHm58hWjIGdOJY";
+    let unencodedApiKey = accessToken + "@" + merchantNumber + ":" + secretToken;
+
+    let encodedApiKey = btoa(unencodedApiKey);
+
+    let checkoutEndpoint = "https://api.v1.checkout.bambora.com/sessions";
+
+    let header = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Basic ' + encodedApiKey
+    }
+
+    let data = JSON.stringify({
+        "order": {
+            "id": "Order123",
+            "amount": 375,
+            "currency": "SEK"
+        },
+        "url": {
+            "accept": "https://example.org/accept",
+            "cancel": "http://localhost:4200/inline"
+        }
+    });
+
+    let request = new Request(checkoutEndpoint, {
+        method: 'POST',
+        body: data,
+        headers: new Headers(header)
+    })
+
+
+    fetch(request)
+        .then((response) => response.json())
+        .then((responseJSON) => {
+            this.initWithToken(responseJSON.token);
+            console.log('token: ' + responseJSON.token);
+        });
+  }
+
+  initWithToken = async (token:string) => {
+    console.log('initWithToken');
+    var checkout = new InlineCheckout(
+      token,
+      {
+        container: document.body
+        // For this example, we put the iframe in the body element.
+      }
+    );
+    checkout.on(
+      Event.Authorize,
+      function(payload) {
+        console.log(payload.data.txnid);
+        // The transaction ID is logged to the console upon authorize.
+      }
+    );
+  }
+
+
 }
+
--- CheckoutSDK/src/app/modal/modal.component.ts
+++ CheckoutSDK/src/app/modal/modal.component.ts
@@ -1,15 +1,75 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalCheckout, Event} from "@bambora/checkout-sdk-web";
 
 @Component({
   selector: 'app-modal',
-  templateUrl: './modal.component.html',
+  template: `<button (click)="buttonClicked()">Betala modal</button>`,
   styleUrls: ['./modal.component.css']
 })
 export class ModalComponent implements OnInit {
+  parentUrl:string;
+  constructor(){
+      this.parentUrl='empty url'
+   }
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit(){
   }
+
+  buttonClicked() {
+    let accessToken = "M09TiRC9emeAdBYAdbhV";
+    let merchantNumber = "T064742601";
+    let secretToken = "yfxJkjkGexrWbmYBrb8hVrsAVWjHm58hWjIGdOJY";
+    let unencodedApiKey = accessToken + "@" + merchantNumber + ":" + secretToken;
+
+    let encodedApiKey = btoa(unencodedApiKey);
+
+    let checkoutEndpoint = "https://api.v1.checkout.bambora.com/sessions";
+
+    let header = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Basic ' + encodedApiKey
+    }
+
+    let data = JSON.stringify({
+        "order": {
+            "id": "Order123",
+            "amount": 375,
+            "currency": "SEK"
+        },
+        "url": {
+            "accept": "https://example.org/accept",
+            "cancel": "http://localhost:4200/modal"
+        }
+    });
+
+    let request = new Request(checkoutEndpoint, {
+        method: 'POST',
+        body: data,
+        headers: new Headers(header)
+    })
+
+
+    fetch(request)
+        .then((response) => response.json())
+        .then((responseJSON) => {
+            this.initWithToken(responseJSON.token);
+            console.log('token: ' + responseJSON.token);
+        });
+  }
+
+  initWithToken = async (token: string) => {
+    const checkout = new ModalCheckout(token);
+    checkout.on(
+        Event.Authorize,
+        function (payload) {
+            console.log(payload.data.txnid);
+            // The transaction ID is logged to the console upon authorize.
+        }
+    );
+
+    checkout.show();
+}
+
 
 }
